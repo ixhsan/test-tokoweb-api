a8068757274492d25fd7d4b9d40cea97dc86ec18
ixhsan <muharipan@gmail.com>
2023-05-25 20:41:34 +0700

    removed unused lines and components

--- app/components/buttons.component.tsx
+++ app/components/buttons.component.tsx
@@ -11,26 +11,10 @@ export const LoginButton = () => {
   );
 };
 
-export const RegisterButton = () => {
-  return (
-    <div className="mr-5">
-      <Link className="p-4 bg-blue-500 text-white rounded-lg px-8" href="/register">Register</Link>
-    </div>
-  );
-};
-
 export const LogoutButton = () => {
   return (
     <div className="mr-5">
       <button className="px-6 py-4 bg-orange-500 text-white rounded-lg" onClick={() => signOut()}>Sign Out</button>
-    </div>
-  );
-};
-
-export const ProfileButton = () => {
-  return (
-    <div className="mr-5">
-      <Link className="p-4 bg-blue-500 text-white rounded-lg px-8" href="/profile">Profile</Link>
     </div>
   );
 };
